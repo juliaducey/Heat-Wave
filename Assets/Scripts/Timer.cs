7c6f5e4928ae469a7aec3fd57d5484aea861c55d
rwedge <rwedge@mit.edu>
2014-10-29 01:48:10 -0400

    added timer component that gets instatiated at the start screen

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    public float time;
+    public bool state;
+	// Use this for initialization
+	void Start () {
+        time = 0f;
+        state = false;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (state)
+        {
+            time += Time.deltaTime;
+        }
+	
+	}
+
+    public void StartTimer()
+    {
+        state = true;
+    }
+
+    public void StopTimer()
+    {
+        state = false;
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+
+    public void SetTime(float t)
+    {
+        time = t;
+    }
+}
