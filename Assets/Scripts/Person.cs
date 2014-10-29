dde5587fa72c203aad43faecaf13865de16ec2e0
rwedge <rwedge@mit.edu>
2014-10-29 15:14:21 -0400

    made a timer prefab

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -8,20 +8,19 @@ public class Person : MonoBehaviour {
 	public bool old;
 	public int timeOutside = 0; // use our global timer, rather than updating on every frame
 	public int timeTillFaint; // possibly calculate from bool values
-	private float startTime;
+	//private float startTime;
 
 	// Use this for initialization
 	void Start () {
 		this.male = Random.Range(0.0F, 1.0F) > .5;
 		this.drunk = Random.Range(0.0F, 1.0F) > .75;
 		this.old = Random.Range(0.0F, 1.0F) > .75;
-		this.startTime = Time.time;
+		//this.startTime = Time.time;
 		// one turn for each risk factor + a random int from 0 to 3
 		this.timeTillFaint = 3 - (this.male.GetHashCode() + this.drunk.GetHashCode() + this.old.GetHashCode()) + (int)Random.Range (0, 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+    public GameObject timer;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    void Awake()
+    {
+        if (!GameObject.Find("Timer(Clone)"))
+            Instantiate(timer, Vector3.zero, Quaternion.identity);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
