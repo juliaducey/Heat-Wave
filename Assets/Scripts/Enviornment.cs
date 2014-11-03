87b4d650f876737719a2ca0a7cb333ff3c238d9d
Megan Gebhard <mgebhard1995@gmail.com>
2014-11-03 14:48:52 -0500

    Integrated Enviornment script

--- Assets/Scripts/Enviornment.cs
+++ Assets/Scripts/Enviornment.cs
@@ -2,28 +2,31 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Temperature : MonoBehaviour {
+public class Enviornment : MonoBehaviour {
 	public float temperature;
 	public Timer timerObject;
 	public int currentDay = 1;
 	public Text text; 
+	public int numberOfPeopleDead = 0;
+	public int numberOfPeopleInside = 0;
+	
 	// Use this for initialization
 	void Start () {
 		text = GetComponent <Text> ();
 		temperature = Random.Range(80.0f, 120.0f);
 		text.text = "Temperature: " + temperature.ToString();
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		updateTemperature ();
 	}
-
+	
 	public float getTemperature() {
 		return temperature;
 	}
-
+	
 	public void updateTemperature() {
 		timerObject = GameObject.Find ("Timer").GetComponent<Timer> ();
 		if (timerObject.day > currentDay) {
@@ -31,5 +34,13 @@ public class Temperature : MonoBehaviour {
 			temperature = Random.Range(80.0F, 120.0F);
 			text.text = "Temperature: " + temperature.ToString();
 		}
+	}
+	
+	public void updateNumberOfPeopleDead(int numberFainted) {
+		numberOfPeopleDead += numberFainted;
+	}
+	
+	public void updateNumberOfPeopleInside(int numberThatWentInside) {
+		numberOfPeopleInside += numberThatWentInside;
 	}
 }
