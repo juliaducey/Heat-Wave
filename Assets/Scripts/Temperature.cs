ecf73f72eb68f54b3812bc78fbe6e194a2212e39
Megan Gebhard <mgebhard1995@gmail.com>
2014-11-03 13:56:23 -0500

    Update Temperature UI

--- Assets/Scripts/Temperature.cs
+++ Assets/Scripts/Temperature.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Temperature : MonoBehaviour {
-	public float temperature = 85.4f;
+	public float temperature;
 	public Timer timerObject;
 	public int currentDay = 1;
+	public Text text; 
 	// Use this for initialization
 	void Start () {
+		text = GetComponent <Text> ();
+		temperature = Random.Range(80.0f, 120.0f);
+		text.text = "Temperature: " + temperature.ToString();
 	
 	}
 	
@@ -24,7 +29,7 @@ public class Temperature : MonoBehaviour {
 		if (timerObject.day > currentDay) {
 			currentDay = timerObject.day;
 			temperature = Random.Range(80.0F, 120.0F);
-			Debug.Log(temperature);
+			text.text = "Temperature: " + temperature.ToString();
 		}
 	}
 }
