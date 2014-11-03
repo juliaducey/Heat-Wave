c70f09e974a306d1fe3632961da0457fc363bfd3
Megan Gebhard <mgebhard1995@gmail.com>
2014-11-03 13:23:40 -0500

    Added random temperature change on a new day and fixed the timer so it is formatted in days, hours, minutes, seconds.

--- Assets/Scripts/Temperature.cs
+++ Assets/Scripts/Temperature.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class Temperature : MonoBehaviour {
-
+	public float temperature = 85.4f;
+	public Timer timerObject;
+	public int currentDay = 1;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,19 @@ public class Temperature : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		updateTemperature ();
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public void updateTemperature() {
+		timerObject = GameObject.Find ("Timer").GetComponent<Timer> ();
+		if (timerObject.day > currentDay) {
+			currentDay = timerObject.day;
+			temperature = Random.Range(80.0F, 120.0F);
+			Debug.Log(temperature);
+		}
 	}
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class Timer : MonoBehaviour {
-    public float time;
-    public bool state;
+	public int day, hours, minutes;
+	public bool state;
+	float seconds;
+	int speed;
+
 	// Use this for initialization
 	void Start () {
-        time = 0f;
-        state = false;
+		hours   = 0;
+		minutes = 0;
+		seconds = 0;
+		day     = 1;
+		speed   = 100;
+        state = true;
         DontDestroyOnLoad(gameObject);
 	}
 	
@@ -15,7 +22,21 @@ public class Timer : MonoBehaviour {
 	void Update () {
         if (state)
         {
-            time += Time.deltaTime;
+			seconds += Time.deltaTime * speed;
+			
+			if (seconds >= 1) {
+				minutes += 1;
+				seconds = 0;
+			}
+			if (minutes >= 60) {
+				hours += 1;
+				minutes = 0;
+			}
+			
+			if (hours >= 24) {
+				hours = 0;
+				day += 1;
+			}
         }
 	
 	}
@@ -32,11 +53,11 @@ public class Timer : MonoBehaviour {
 
     public float GetTime()
     {
-        return time;
+        return seconds;
     }
 
     public void SetTime(float t)
     {
-        time = t;
+        seconds = t;
     }
 }
