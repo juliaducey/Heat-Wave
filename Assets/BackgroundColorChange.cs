327a74f946dd744cad86b5db68509e129215f33c
Julia Ducey <jducey@mit.edu>
2014-12-08 19:50:13 -0500

    colors aren't broken now

--- Assets/BackgroundColorChange.cs
+++ Assets/BackgroundColorChange.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class BackgroundColorChange : MonoBehaviour {
 
 	public SpriteRenderer Background;
-	public Timer Timer;
+	Timer Timer;
 
 	public Color DawnColor;
 	public Color NightColor;
 
 	private const float TOTAL_MINS = 60*16f;
-	
+
+	void Start()
+	{
+		Timer = (Timer) GameObject.FindObjectOfType<Timer>();
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
